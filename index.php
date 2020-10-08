91797b29c4e99b85b04e5d563ba6a6c614dc6614
esdu <3723esad@gmail.com>
2020-10-08 09:36:41 +0200

    data from api turned to json file

--- index.php
+++ index.php
@@ -1,4 +1,15 @@
 <?php
-    $content = file_get_contents("https://api.thedogapi.com/v1/breeds");
-    var_dump($content);
-?>
+
+    //__Get dog breeds from API and turn it __//
+    //__to JSON file and save it to directory__//
+    
+    // // Get CSV file content
+    // $content = file_get_contents("https://api.thedogapi.com/v1/breeds");
+
+    // // Upload Directory
+    // $upload_dir = "/Applications/XAMPP/xamppfiles/htdocs/My-Works/dog-breeds-project/";
+
+    // // Create new file and write into
+    // $fp = fopen($upload_dir."dog-breeds.JSON", "w");
+    // fwrite($fp, $content);
+    // fclose($fp);
