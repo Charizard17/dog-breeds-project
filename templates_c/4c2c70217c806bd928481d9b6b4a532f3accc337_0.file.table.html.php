d42eea85bd61830e3ad7179d20e41ae59a715bf1
Esad Dursun <3723esad@gmail.com>
2020-12-16 17:10:45 +0100

    table, th, tr, td class name assigned and page overflow added

--- templates_c/4c2c70217c806bd928481d9b6b4a532f3accc337_0.file.table.html.php
+++ templates_c/4c2c70217c806bd928481d9b6b4a532f3accc337_0.file.table.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.36, created on 2020-12-16 15:13:13
+/* Smarty version 3.1.36, created on 2020-12-16 15:39:04
   from '/Applications/XAMPP/xamppfiles/htdocs/My-Works/dog-breeds-project/templates/table/table.html' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.36',
-  'unifunc' => 'content_5fda2409bc6611_89810204',
+  'unifunc' => 'content_5fda2a18017845_03679809',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '4c2c70217c806bd928481d9b6b4a532f3accc337' => 
     array (
       0 => '/Applications/XAMPP/xamppfiles/htdocs/My-Works/dog-breeds-project/templates/table/table.html',
-      1 => 1608131592,
+      1 => 1608133126,
       2 => 'file',
     ),
   ),
@@ -20,18 +20,18 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_5fda2409bc6611_89810204 (Smarty_Internal_Template $_smarty_tpl) {
+function content_5fda2a18017845_03679809 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_checkPlugins(array(0=>array('file'=>'/Applications/XAMPP/xamppfiles/htdocs/My-Works/dog-breeds-project/libs/plugins/function.html_image.php','function'=>'smarty_function_html_image',),));
 ?>
-<table class="table">
+<table class="dog-breeds-table">
     <thead>
       <tr>
-        <th scope="col">ID</th>
-        <th scope="col">Name</th>
-        <th scope="col">Picture</th>
-        <th scope="col">Weight(kg)</th>
-        <th scope="col">Height(cm)</th>
-        <th scope="col">Temperament</th>
+        <th class="dog-breeds-table-th" scope="col">ID</th>
+        <th class="dog-breeds-table-th" scope="col">Name</th>
+        <th class="dog-breeds-table-th" scope="col">Picture</th>
+        <th class="dog-breeds-table-th" scope="col">Weight(kg)</th>
+        <th class="dog-breeds-table-th" scope="col">Height(cm)</th>
+        <th class="dog-breeds-table-th" scope="col">Temperament</th>
       </tr>
     </thead>
     <tbody>
@@ -42,7 +42,7 @@ $_smarty_tpl->tpl_vars['fooitem']->do_else = true;
 if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['fooitem']->value) {
 $_smarty_tpl->tpl_vars['fooitem']->do_else = false;
 ?>
-            <tr>
+            <tr class="dog-breeds-table-tr">
             <?php
 $_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['fooitem']->value, 'item', false, 'key');
 $_smarty_tpl->tpl_vars['item']->do_else = true;
@@ -51,13 +51,13 @@ $_smarty_tpl->tpl_vars['item']->do_else = false;
 ?>
                 <?php if ($_smarty_tpl->tpl_vars['key']->value == 'id' || $_smarty_tpl->tpl_vars['key']->value == 'breed_name' || $_smarty_tpl->tpl_vars['key']->value == 'img' || $_smarty_tpl->tpl_vars['key']->value == 'weight' || $_smarty_tpl->tpl_vars['key']->value == 'height' || $_smarty_tpl->tpl_vars['key']->value == 'temperament') {?>
                   <?php if ($_smarty_tpl->tpl_vars['key']->value == 'img') {?>
-                    <td><?php echo smarty_function_html_image(array('file'=>$_smarty_tpl->tpl_vars['item']->value,'width'=>"64",'height'=>"64"),$_smarty_tpl);?>
+                    <td class="dog-breeds-table-td"><?php echo smarty_function_html_image(array('file'=>$_smarty_tpl->tpl_vars['item']->value,'width'=>"64",'height'=>"64"),$_smarty_tpl);?>
 </td>
                   <?php } elseif ($_smarty_tpl->tpl_vars['key']->value == 'weight' || $_smarty_tpl->tpl_vars['key']->value == 'height') {?>
-                    <td><?php echo $_smarty_tpl->tpl_vars['item']->value['metric'];?>
+                    <td class="dog-breeds-table-td"><?php echo $_smarty_tpl->tpl_vars['item']->value['metric'];?>
 </td>
                   <?php } else { ?>
-                    <td><?php echo $_smarty_tpl->tpl_vars['item']->value;?>
+                    <td class="dog-breeds-table-td"><?php echo $_smarty_tpl->tpl_vars['item']->value;?>
 </td>
                   <?php }?>
                 <?php }?>
