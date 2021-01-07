501e96adb798cae93175f1fc7d65e24a38270d69
Esad Dursun <3723esad@gmail.com>
2021-01-07 15:25:01 +0100

    initial temp arrays in search function moved out of ifs

--- includes/search.php
+++ includes/search.php
@@ -17,9 +17,9 @@
 
     // we create 2 functions for filtering our array after search
     function hasContainWords($postValue, $dataArray, $cookieName) {
+        $dataArrayTemp = [];
         $index2 = 0;
         if ($_POST[$postValue]) {
-            $dataArrayTemp = [];
             if (is_array($dataArray)){
                 for ($i = 0; $i < count($dataArray); ++$i) {
                     if (strpos(strtolower($dataArray[$i][$postValue]), strtolower($_POST[$postValue])) !== false) {
@@ -34,9 +34,9 @@
         return $dataArray;
     }
     function hasContainTerms($postValue, $dataArray, $dataArrayFirstIndex, $dataArraySecondIndex, $cookieName) {
+        $dataArrayTemp = [];
+        $index3 = 0;
         if ($_POST[$postValue]) {
-            $dataArrayTemp = [];
-            $index3 = 0;
             if (is_array($dataArray)){
                 for ($i = 0; $i < count($dataArray); ++$i) {
                     if ($_POST[$postValue] >= $dataArray[$i][$dataArrayFirstIndex] && $_POST[$postValue] <= $dataArray[$i][$dataArraySecondIndex]) {
