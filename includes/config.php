39b15807b7952c66076361027a051ccd5b882694
Esad Dursun <3723esad@gmail.com>
2021-01-25 17:48:01 +0100

    creating json file part modified

--- includes/config.php
+++ includes/config.php
@@ -5,6 +5,23 @@
         // Get CSV file content
         $content = file_get_contents("https://api.thedogapi.com/v1/breeds");
 
+        // Turn content string to an array
+        $contentArray = json_decode($content, true);
+
+        // reduce image size from api, before creating JSON file
+        for ($i = 0; $i < count($contentArray); ++$i) {
+            if ($contentArray[$i]['image']['width']) {
+                $contentArray[$i]['image']['width'] = 250;
+            }
+            if ($contentArray[$i]['image']['height']) {
+                $contentArray[$i]['image']['height'] = 250;
+            }
+        }
+
+        // Turn content array back to string
+        $content = json_encode($contentArray, true);
+
+
         // Upload Directory
         $upload_dir = "/Applications/XAMPP/xamppfiles/htdocs/My-Works/dog-breeds-project/";
 
